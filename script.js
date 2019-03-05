80126439a6263411d789e2f8fb7cab3102cc494b
VitalGol <vital_gol@ukr.net>
2019-03-05 11:56:35 +0200

    add filters

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const sepia = document.querySelector('#sepia');
 const saturate = document.querySelector('#saturate');
 const img = document.querySelector('#photo');
 
+const defaults = {
+	grayscale: 0,
+	contrast: 100,
+	brightness: 100,
+	sepia: 0,
+	saturate: 100
+}
+
 
 grayscale.addEventListener('input', updateFilterValue);
 contrast.addEventListener('input', updateFilterValue);
@@ -14,9 +22,12 @@ saturate.addEventListener('input', updateFilterValue);
 
 
 function updateFilterValue() {
-	console.log(grayscale.value);
-	console.log(contrast.value);
-	console.log(brightness.value);
-	console.log(sepia.value);
-	console.log(saturate.value);
+	reset.disabled = false;
+	img.style.filter = `
+	grayscale(${grayscale.value}%)
+	contrast(${contrast.value}%)
+	brightness(${brightness.value}%)
+	sepia(${sepia.value}%)
+	saturate(${saturate.value}%)
+	`
 }
